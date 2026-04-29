5e0a83c6b576d190d8cc7fe8eef783779dc94605
Severus Snape <snape@drunkleen.com>
2026-04-30 00:33:12 +0200

    chore: add migrations for 2026-04-29 and 2026-04-30
    - Allow Docker DNS from 192.168.0.0/16 pool via UFW
    - Hide shutdown console messages behind Plymouth
    - Switch to leenium UWSM wayland session
    - Install dosfstools
    - Rename lock-screen cmd in hypridle config

---
+++ migrations/20260429-allow-docker-dns-from-192168-pool.sh
@@ -0,0 +1,6 @@
+echo "Allow Docker DNS from Docker's 192.168 address pool"
+MIGRATION_VERSION="1.2.0"
+
+if leenium-cmd-present ufw && sudo ufw status | grep -q "Status: active"; then
+  sudo ufw allow in proto udp from 192.168.0.0/16 to 172.17.0.1 port 53 comment 'allow-docker-dns'
+fi
