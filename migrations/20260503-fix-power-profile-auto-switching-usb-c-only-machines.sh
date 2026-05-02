c8b8c8b91c2daef74d0255037ed822a092c4290a
Severus Snape <snape@drunkleen.com>
2026-05-03 01:45:35 +0200

    chore: add 1.2.0 update migrations for power and limine

---
+++ migrations/20260503-fix-power-profile-auto-switching-usb-c-only-machines.sh
@@ -0,0 +1,4 @@
+echo "Fix power profile auto-switching on USB-C only machines and ensure power-profiles-daemon is enabled"
+MIGRATION_VERSION="1.2.0"
+
+source "$LEENIUM_PATH/install/config/powerprofilesctl-rules.sh"
